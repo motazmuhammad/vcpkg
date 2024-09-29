f0a701790ddb751a17973c6b8531f34a206283b0
motazmuhammad <motaz.muhammad@kaust.edu.sa>
2024-09-29 12:25:43 +0100

    first version
    no features, usage, and no version yet

--- ports/pdf-writer/portfile.cmake
+++ ports/pdf-writer/portfile.cmake
@@ -1,23 +1,35 @@
-# Header-only library
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO troldal/OpenXLSX
+    REPO galkahana/PDF-Writer
     REF master
-    SHA512 e3d27b6e21dcb727398ec1c78cdde4adfb9d5d354380e22217f63c46dc63a1dcdfde54733b70ac9b7e5fa7f6bc47028b311f03fdccc980ed9132ac0a23a23ed4
+    SHA512 f9cc60f8c9a8832a111abb4eb80818c1ddc4a5f8bb7954b83964be65b70d92932bb2e034c63c24d8c574d0183aa852b60baf829df12d7e3e6bec81c5d5108c89
     HEAD_REF master
 )
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-        -DOPENXLSX_BUILD_TESTS=OFF
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+FEATURES
+    "nopng"                 PDFHuMMUS_NO_PNG  
+    "nojpeg"                PDFHUMMUS_NO_DCT  
+    "notiff"                PDFHUMMUS_NO_TIFF 
+    "bundled"               USE_BUNDLED 
+    "fuzz"                  BUILD_FUZZING_HARNESS 
 )
-vcpkg_install_cmake()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/OpenXLSX)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    # ${FEATURE_OPTIONS}
+
+)
+
+
+vcpkg_cmake_install()
+ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/PDFHummus)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_copy_pdbs()
 
 
 # Handle copyright
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+# file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
