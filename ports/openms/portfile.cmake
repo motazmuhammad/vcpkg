c682661fbfe4884d258a335bbe58a0c34c8bfacd
motazmuhammad <motaz.muhammad@kaust.edu.sa>
2024-10-08 22:44:46 +0100

    a second version (static) not working because of messsing dependencies

--- ports/openms/portfile.cmake
+++ ports/openms/portfile.cmake
@@ -1,10 +1,10 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+# vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO OpenMS/OpenMS
-  REF "${VERSION}"
-  SHA512 0
+  REF "refs/tags/release/${VERSION}"
+  SHA512 65ee28f0b18d2f960b732f044accafee962fa9fbded07484c809f0f37db04109b0ddfe1c635650a3f38db2b8a30d1db4c742f51faa43b75021c86ead105e42c8
   HEAD_REF master
 #  PATCHES
 #    fix-cmake.patch
